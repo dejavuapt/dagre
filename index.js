ec89da0f50ccb5f03a5907dc9553595c78dbb825
xdashduck <xdashduck@debian-laptop.tendawifi.com>
2024-03-23 20:01:15 +0500

    add to layout in,out position couning

--- index.js
+++ index.js
@@ -31,3 +31,43 @@ module.exports = {
   },
   version: require("./lib/version")
 };
+
+var graphlib = require("@dagrejs/graphlib")
+var g = new graphlib.Graph();
+g.setNode("a");
+console.log(g.hasNode("a"));
+
+var gd = new graphlib.Graph({directed: true}).setGraph({rankdir:"LR"});
+gd.setDefaultEdgeLabel(function(){ return {width:50};});
+// оно реверсится... из-за LR, width = height, height = width.
+gd.setNode("1", {label: "Su", width: 1000, height: 0, in: 2, out: 4}); // название портов всегда есть
+gd.setNode("2", {label: "I1", width: 30, height: 0});
+gd.setNode("3", {label: "I2", width: 30, height: 0});
+gd.setNode("4", {label: "R", width: 30, height: 40});
+gd.setNode("5", {label: "D", width: 30, height: 40});
+
+gd.setEdge("1", "2");
+gd.setEdge("1", "3");
+gd.setEdge("3", "4");
+gd.setEdge("2", "4");
+gd.setEdge("3", "5");
+gd.setEdge("1", "5");
+gd.setEdge("2", "1");
+
+console.log({edges: gd.edges()})
+
+// normalize - это то что создает фиктивные узлы
+
+var layout = require("./lib/layout")
+layout(gd, {minlen:0, ranker:"longest-path"});
+gd.edges().forEach(function(e) {
+  var points = [];
+  gd.edge(e).points.forEach((p)=>{
+    points.push(p.x, p.y);
+  });
+  console.log(points)
+ console.log("Edge " + e.v + " -> " + e.w + ": " + JSON.stringify(gd.edge(e)));
+});
+gd.nodes().forEach(function(v) {
+  console.log("Node " + v + ": " + JSON.stringify(gd.node(v)));
+});
